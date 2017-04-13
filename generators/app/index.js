0dc77fa6acbb91dd88eea6d2e98957dd1e9bae2e
zhanziyang <zhanziyang126@hotmail.com>
2017-04-13 15:31:23 +0800

    change postcss to stylus

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,62 +1,61 @@
-var generators = require("yeoman-generator");
-var initialPropmts = require("./initial_prompt");
-var mkdirp = require("mkdirp");
-var _ = require("underscore");
+var generators = require('yeoman-generator')
+var initialPropmts = require('./initial_prompt')
+var mkdirp = require('mkdirp')
+var _ = require('underscore')
 
 var copyTpl = function (tpl, dest, vars) {
-  var map = {};
+  var map = {}
   vars.forEach(function (v) {
-    map[v] = this.answers[v];
-  }.bind(this));
+    map[v] = this.answers[v]
+  }.bind(this))
 
-  this.fs.copyTpl(this.templatePath(tpl), this.destinationPath(dest), map);
-};
+  this.fs.copyTpl(this.templatePath(tpl), this.destinationPath(dest), map)
+}
 
 var copyFiles = function (srcs, dests) {
   if (!(_.isArray(srcs) && _.isArray(dests)) && !(_.isString(srcs) && _.isString(dests))) {
-    this.env.error("Argument type is wrong");
-    return;
+    this.env.error('Argument type is wrong')
+    return
   }
   if (_.isString(srcs) && _.isString(dests)) {
-    srcs = [srcs];
-    dests = [dests];
+    srcs = [srcs]
+    dests = [dests]
   }
   if (srcs.length != dests.length) {
-    this.env.error("Invalid copy plan. You have " + srcs.length + " source file path" + (srcs.length > 1 ? "s" : "") + " and " + dests.length + " destination file path" + (srcs.length > 1 ? "s" : "") + " in your plan. Make sure they are equal!");
-    return;
+    this.env.error('Invalid copy plan. You have ' + srcs.length + ' source file path' + (srcs.length > 1 ? 's' : '') + ' and ' + dests.length + ' destination file path' + (srcs.length > 1 ? 's' : '') + ' in your plan. Make sure they are equal!')
+    return
   }
   for (var i = 0; i < srcs.length; i++) {
-    this.fs.copy(this.templatePath(srcs[i]), this.destinationPath(dests[i]));
+    this.fs.copy(this.templatePath(srcs[i]), this.destinationPath(dests[i]))
   }
-};
+}
 
 var mkdirs = function (dirs) {
   for (var i = 0; i < dirs.length; i++) {
-    mkdirp.sync(this.destinationPath(dirs[i]));
+    mkdirp.sync(this.destinationPath(dirs[i]))
   }
-};
+}
 
 module.exports = generators.Base.extend({
   constructor: function () {
-    generators.Base.apply(this, arguments);
+    generators.Base.apply(this, arguments)
   },
 
   initializing: function () {
-    this.log("OK ladies now let's get in Formation...");
-    this.log("initializing...in " + this.destinationRoot());
-    this.log("template in " + this.sourceRoot());
+    this.log('initializing...in ' + this.destinationRoot())
+    this.log('template in ' + this.sourceRoot())
   },
 
   prompting: function () {
-    var done = this.async();
+    var done = this.async()
     this.prompt(initialPropmts, function (answers) {
-      this.answers = answers;
-      done();
-    }.bind(this));
+      this.answers = answers
+      done()
+    }.bind(this))
   },
 
   configuring: function () {
-    this.log("configuring...");
+    this.log('configuring...')
   },
 
   default: function () {
@@ -66,64 +65,60 @@ module.exports = generators.Base.extend({
   writing: function () {
     //创建基本目录结构
     var dirs = [
-      "dist",
-      "src",
-      "dist/css",
-      "dist/images",
-      "dist/js",
-      "src/css"
-    ];
-    mkdirs.call(this, dirs);
+      'dist',
+      'src',
+      'dist/css',
+      'dist/images',
+      'dist/js',
+      'src/css'
+    ]
+    mkdirs.call(this, dirs)
 
     //纯复制
     var pureCopies = {
       srcs: [
-        "dist/css/normalize.css",
-        "dist/js/app.js",
-        "src/css/!(util.css)",
-        ".eslintrc.json",
-        ".stylelintrc.json",
-        "cssconfig.js",
-        "jsconfig.json",
-        "package.json"
+        'dist/css/normalize.css',
+        'dist/js/app.js',
+        'src/css/!(util.css)',
+        '.eslintrc.json',
+        'jsconfig.json',
+        'package.json'
       ],
       dests: [
-        "dist/css/normalize.css",
-        "dist/js/app.js",
-        "src/css",
-        ".eslintrc.json",
-        ".stylelintrc.json",
-        "cssconfig.js",
-        "jsconfig.json",
-        "package.json"
+        'dist/css/normalize.css',
+        'dist/js/app.js',
+        'src/css',
+        '.eslintrc.json',
+        'jsconfig.json',
+        'package.json'
       ]
-    };
+    }
     if (this.answers.$) {
-      pureCopies.srcs.push("dist/js/jquery-2.1.4.min.js");
-      pureCopies.dests.push("dist/js/jquery-2.1.4.min.js");
+      pureCopies.srcs.push('dist/js/jquery-2.1.4.min.js')
+      pureCopies.dests.push('dist/js/jquery-2.1.4.min.js')
     }
     if (this.answers.noBounce) {
-      pureCopies.srcs.push("dist/js/inobounce.js");
-      pureCopies.dests.push("dist/js/inobounce.js");
+      pureCopies.srcs.push('dist/js/inobounce.js')
+      pureCopies.dests.push('dist/js/inobounce.js')
     }
-    copyFiles.call(this, pureCopies.srcs, pureCopies.dests);
+    copyFiles.call(this, pureCopies.srcs, pureCopies.dests)
 
     //复制模板
-    copyTpl.call(this, "dist/index.html", "dist/index.html", ["title", "$", "accurateVW", "vw", "noBounce"]);
-    copyTpl.call(this, "src/css/util.css", "src/css/util.css", ["vw", "accurateVW"]);
+    copyTpl.call(this, 'dist/index.html', 'dist/index.html', ['title', '$', 'accurateVW', 'vw', 'noBounce'])
+    copyTpl.call(this, 'src/css/util.styl', 'src/css/util.styl', ['vw', 'accurateVW'])
   },
 
   install: function () {
-    var SBP = this.answers.SASS_BINARY_PATH;
-    process.env.SASS_BINARY_PATH = SBP;
-    this.spawnCommandSync("npm", ["init"]);
-    this.spawnCommandSync("npm", ["install"]);
-    this.log("Let's get the party started!");
-    this.spawnCommandSync("npm", ["start"]);
+    this.spawnCommandSync('npm', ['init'], {
+      yes: true
+    })
+    this.spawnCommandSync('npm', ['install'])
+    this.log('The party is getting started...')
+    this.spawnCommandSync('npm', ['start'])
   },
 
   end: function () {
-    this.log("THE END");
+    this.log('THE END')
   }
 
-});
+})
--- generators/app/initial_prompt.js
+++ generators/app/initial_prompt.js
@@ -1,38 +1,32 @@
 module.exports = [
   {
-    type: "input",
-    name: "title",
-    message: "What is the title of the index.html file?",
-    default: "首页"
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of the index.html file?',
+    default: '首页'
   },
   {
-    type: "confirm",
-    name: "$",
-    message: "Do you need JQuery?",
+    type: 'confirm',
+    name: '$',
+    message: 'Do you need JQuery?',
     default: true
   },
   {
-    type: "confirm",
-    name: "noBounce",
-    message: "Do you want to prevent page bounce on IOS?",
+    type: 'confirm',
+    name: 'noBounce',
+    message: 'Do you want to prevent page bounce on IOS?',
     default: false
   },
   {
-    type: "confirm",
-    name: "accurateVW",
-    message: "Do you want to use a specific fixed viewport width?",
+    type: 'confirm',
+    name: 'accurateVW',
+    message: 'Do you want to use a specific fixed viewport width?',
     default: true
   },
   {
-    type: "input",
-    name: "vw",
-    message: "Set the viewport width if the last answer is true",
-    default: "640"
-  },
-  {
-    type: "input",
-    name: "SASS_BINARY_PATH",
-    message: "SASS_BINARY_PATH separated by \"\\\":",
-    default: "C:\\Users\\zhanz\\win32-ia32-46_binding.node"
+    type: 'input',
+    name: 'vw',
+    message: 'Set the viewport width if the last answer is true',
+    default: '640'
   }
 ]
